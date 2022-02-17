27102077b56475a6b201a8dc966993cea5d24408
Thomas <venturini.thomas95@gmail.com>
2022-02-17 21:34:54 +0100

    finito (no bonus)

--- js/main.js
+++ js/main.js
@@ -1,34 +1,62 @@
+const app = new Vue (
 
+{
 
-const items = [
-    {
-        url:'img/01.jpg',
-        name:'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    el:"#root",
+    data: {
+        items : [
+            {
+                url:'img/01.jpg',
+                name:'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                url:'img/02.jpg',
+                name:'Svizzera',
+                text:'Lorem ipsum'
+            },
+            {
+                url:'img/03.jpg',
+                name:'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                url:'img/04.jpg',
+                name:'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },
+            {
+                url:'img/05.jpg',
+                name:'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            }
+        ],
+        activeSlide : 0,
+
     },
-    {
-        url:'img/02.jpg',
-        name:'Svizzera',
-        text:'Lorem ipsum'
-    },
-    {
-        url:'img/03.jpg',
-        name:'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
-    },
-    {
-        url:'img/04.jpg',
-        name:'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
-    },
-    {
-        url:'img/05.jpg',
-        name:'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+
+    methods: {
+        goToNext() {
+            this.activeSlide++;
+            if(this.activeSlide > this.items.length - 1) {
+                this.activeSlide = 0;
+            }
+        },
+        goToPrev() {
+            this.activeSlide--;
+            if(this.activeSlide < 0 ) {
+                this.activeSlide =  this.items.length - 1;
+            }
+        },
+        goToSlide(index) {
+            this.activeSlide = index;
+        }
     }
-];
+});
 
-const itemsRef = document.getElementsByClassName('items')[0];
+
+
+/* const itemsRef = document.getElementsByClassName('items')[0];
 const thumbsRef = document.getElementsByClassName('thumbs')[0];
 let item = '';
 let thumb = '';
@@ -93,4 +121,4 @@ next.addEventListener('click', function() {
         document.querySelector('.thumb.active').classList.remove('active');
         document.getElementsByClassName('thumb')[active].classList.add('active');
     }
-});
+}); */
